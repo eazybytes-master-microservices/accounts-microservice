812567f6d28abbb12fc2297916454038a289a96e
AngelTravieso <angeltraviesoc@gmail.com>
2024-12-31 21:07:56 -0400

    DELETE API inside accounts microservice

--- src/main/java/com/angeltravieso/accounts/repositories/AccountsRepository.java
+++ src/main/java/com/angeltravieso/accounts/repositories/AccountsRepository.java
@@ -1,7 +1,9 @@
 package com.angeltravieso.accounts.repositories;
 
 import com.angeltravieso.accounts.entity.Accounts;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +12,9 @@ import java.util.Optional;
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
 
     Optional<Accounts> findByCustomerId(Long customerId);
+
+    @Transactional // execute in transaction
+    @Modifying // tell to spring modify data
+    void deleteByCustomerId(Long customerId);
 
 }
